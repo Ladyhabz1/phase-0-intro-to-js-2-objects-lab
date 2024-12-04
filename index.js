04e2bc457fa2170d7d0608b00d5bde38f83fbfc2
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2024-12-04 15:44:28 +0300

    Added functions for updating and deleting employee objects

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+// Initializing the employee Object
+const employee = {
+    name: "Sam"
+};
+// Function to update an employee Object non-destructively
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+// Function to update an employee Object destructively
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; // Mutates the original Object
+    return employee;
+}
+
+// Function to delete a key from an employee Object non-destructively
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee }; // Creates a shallow copy of the Object
+    delete newEmployee[key]; // Removes the key from the new Object
+    return newEmployee;
+}
+
+// Function to delete a key from an employee Object destructively
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; // Mutates the original Object
+    return employee;
+}
+module.exports = {
+    employee,
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey
+};
